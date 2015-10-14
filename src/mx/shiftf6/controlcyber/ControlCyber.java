d8a6361efe10c2d4a7ba00e174a5bee83a36808f
Israel Salazar <josemvp16@gmail.com>
2015-10-14 18:29:48 -0500

    new commit Champe

--- src/mx/shiftf6/controlcyber/ControlCyber.java
+++ src/mx/shiftf6/controlcyber/ControlCyber.java
@@ -12,6 +12,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -30,34 +32,48 @@ public class ControlCyber extends Application {
         this.escenarioPrincipal = primaryStage;
         this.escenarioPrincipal.setTitle("Control de Cyber | Hecho con amor por Shift-F6");
         
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!"); 
-            }
-        });
+        // Configuramos icono de aplicacion
+        this.escenarioPrincipal.getIcons().add(new Image("file:recursos/imagenes/iconos/IconPNG.png"));
         
+        iniciarLayoutRaiz();
+        
+        mostrarPantallaUno();
+    }
+    
+    /**
+     * Inicializamos el layout raiz
+     */
+    public void iniciarLayoutRaiz() {
         try{
+            // Cargamos el layout raiz del archivo fxml
             FXMLLoader cargador = new FXMLLoader();
             cargador.setLocation(ControlCyber.class.getResource("vista/LayoutRaiz.fxml"));
             layoutRaiz = (BorderPane) cargador.load();
+            
+            // Mostramos la escena que contiene el layout raiz
+            Scene scene = new Scene(layoutRaiz);
+            escenarioPrincipal.setScene(scene);
+            escenarioPrincipal.show();
         } catch (IOException ioe) {
             ioe.printStackTrace();
-        }        
-        
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 1440, 900);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+        }// Fin try/catch
+    }// Fin método
+    
+    /**
+     * Mostramos la pantalla uno dentro del layout raíz
+     */
+    public void mostrarPantallaUno() {
+        try{
+            FXMLLoader cargador = new FXMLLoader();
+            cargador.setLocation(ControlCyber.class.getResource("vista/Pantalla1.fxml"));
+            AnchorPane pantallaUno = (AnchorPane) cargador.load();
+            
+            // Colocamos la pantalla uno al centro del layout raíz
+            layoutRaiz.setCenter(pantallaUno);
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }// Fin try/catch
+    }// Fin método
 
     /**
      * @param args the command line arguments
